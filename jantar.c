0a1badac64f3f9e873f344838fbc670d4c139c89
Leonardo Deliyannis Constantin <constantin.leo@gmail.com>
2017-05-01 20:20:39 -0300

    Esqueleto do código

---
+++ jantar.c
@@ -0,0 +1,14 @@
+/*
+ * Leonardo Deliyannis Constantin
+ * jantar.c - uma implementação concorrente do Jantar dos Filósofos
+*/
+
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+
+int main(int argc, char **argv){
+    int N, C, D, tmin, tmax;
+    scanf("%d %d %d %d %d", &N, &C, &D, &tmin, &tmax);
+    return 0;
+}
